48e90d0b0731f66c3a6f39f01604dc4708e08a1a
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-04-25 11:16:45 +0800

    refactor to functional components

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,41 +1,16 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import ColorThief from 'colorthief';
 import Modal from '../Modal/Modal';
 import styles from './ImageGalleryItem.module.css';
-import { useToggle } from 'hooks/useToggle'; // Ensure your useToggle hook is exported from 'hooks/useToggle'
+import { useToggle } from 'hooks/useToggle';
 
 const ImageGalleryItem = ({ image }) => {
   const { isOpen, toggle } = useToggle();
-  const [bgColor, setBgColor] = useState('#f0f8ff'); // Default background color
-  const imgRef = useRef(null);
   const { webformatURL, largeImageURL, tags } = image;
 
-  useEffect(() => {
-    const extractColor = () => {
-      const colorThief = new ColorThief();
-      if (imgRef.current && imgRef.current.complete) {
-        try {
-          const dominantColor = colorThief.getColor(imgRef.current);
-          const bgColor = `rgba(${dominantColor.join(', ')}, 0.6)`;
-          setBgColor(bgColor);
-        } catch (error) {
-          console.error('Error extracting color:', error);
-        }
-      }
-    };
-
-    imgRef.current.addEventListener('load', extractColor);
-    return () => imgRef.current.removeEventListener('load', extractColor);
-  }, [webformatURL]); // Dependency on webformatURL to rerun if the image URL changes
-
   return (
-    <li
-      className={styles.galleryItem}
-      onClick={toggle}
-      style={{ backgroundColor: bgColor }}
-    >
-      <img ref={imgRef} src={webformatURL} alt={tags} />
+    <li className={styles.galleryItem} onClick={toggle}>
+      <img src={webformatURL} alt={tags} />
       {isOpen && <Modal image={largeImageURL} tags={tags} onClose={toggle} />}
     </li>
   );
