4adc474a2f20e9f442bdf1425fbdafb96ae57b00
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-04-25 10:47:43 +0800

    refactor

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,76 +1,52 @@
-import React, { Component } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import ColorThief from 'colorthief'; // Import ColorThief
+import ColorThief from 'colorthief';
 import Modal from '../Modal/Modal';
 import styles from './ImageGalleryItem.module.css';
+import { useToggle } from 'hooks/useToggle'; // Ensure your useToggle hook is exported from 'hooks/useToggle'
 
-class ImageGalleryItem extends Component {
-  static propTypes = {
-    image: PropTypes.shape({
-      webformatURL: PropTypes.string.isRequired,
-      largeImageURL: PropTypes.string.isRequired,
-      tags: PropTypes.string,
-    }).isRequired,
-  };
+const ImageGalleryItem = ({ image }) => {
+  const { isOpen, toggle } = useToggle();
+  const [bgColor, setBgColor] = useState('#f0f8ff'); // Default background color
+  const imgRef = useRef(null);
+  const { webformatURL, largeImageURL, tags } = image;
 
-  state = {
-    showModal: false,
-    bgColor: '#f0f8ff', // Default background color
-  };
-
-  imgRef = React.createRef(); // Create a ref for the image
-
-  componentDidMount() {
-    this.setColor();
-  }
-
-  setColor = () => {
-    if (this.imgRef.current.complete) {
-      // Ensure the image is loaded
-      this.extractColor();
-    } else {
-      this.imgRef.current.addEventListener('load', this.extractColor); // Extract color once the image is loaded
-    }
-  };
-
-  extractColor = () => {
-    const colorThief = new ColorThief();
-    const img = this.imgRef.current;
-    // Make sure the image is not from a different origin to avoid CORS issues
-    if (img && img.src && img.src.startsWith('http')) {
-      try {
-        const dominantColor = colorThief.getColor(img);
-        const bgColor = `rgba(${dominantColor.join(', ')}, 0.6)`; // Set background color with some transparency
-        this.setState({ bgColor });
-      } catch (error) {
-        console.error('Error extracting color:', error);
+  useEffect(() => {
+    const extractColor = () => {
+      const colorThief = new ColorThief();
+      if (imgRef.current && imgRef.current.complete) {
+        try {
+          const dominantColor = colorThief.getColor(imgRef.current);
+          const bgColor = `rgba(${dominantColor.join(', ')}, 0.6)`;
+          setBgColor(bgColor);
+        } catch (error) {
+          console.error('Error extracting color:', error);
+        }
       }
-    }
-  };
+    };
 
-  toggleModal = () => {
-    this.setState(prevState => ({
-      showModal: !prevState.showModal,
-    }));
-  };
+    imgRef.current.addEventListener('load', extractColor);
+    return () => imgRef.current.removeEventListener('load', extractColor);
+  }, [webformatURL]); // Dependency on webformatURL to rerun if the image URL changes
 
-  render() {
-    const { webformatURL, largeImageURL, tags } = this.props.image;
-    const { showModal, bgColor } = this.state;
+  return (
+    <li
+      className={styles.galleryItem}
+      onClick={toggle}
+      style={{ backgroundColor: bgColor }}
+    >
+      <img ref={imgRef} src={webformatURL} alt={tags} />
+      {isOpen && <Modal image={largeImageURL} tags={tags} onClose={toggle} />}
+    </li>
+  );
+};
 
-    return (
-      <li
-        className={styles.galleryItem}
-        onClick={this.toggleModal}
-        style={{ backgroundColor: bgColor }}
-      >
-        <img ref={this.imgRef} src={webformatURL} alt={tags} />
-        {showModal && (
-          <Modal image={largeImageURL} tags={tags} onClose={this.toggleModal} />
-        )}
-      </li>
-    );
-  }
-}
+ImageGalleryItem.propTypes = {
+  image: PropTypes.shape({
+    webformatURL: PropTypes.string.isRequired,
+    largeImageURL: PropTypes.string.isRequired,
+    tags: PropTypes.string,
+  }).isRequired,
+};
 
 export default ImageGalleryItem;
